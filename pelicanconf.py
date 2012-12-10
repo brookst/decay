cf6d80882eefde2ae83c03909ab6271007038e19
Tim Brooks <brooks@cern.ch>
2012-12-10 14:12:07 +0000

    Layout and style tidyup and release

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,7 +33,28 @@ FEED_RSS = 'feeds/all.rss.xml'
 CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
 STATIC_PATHS = ['images','files']
 
+FILES_TO_COPY = (
+        ('extra/.htaccess', '.htaccess'),
+        ('extra/humans.txt', 'humans.txt'),
+        ('extra/robots.txt', 'robots.txt'),
+        )
+
 # Social widget
 SOCIAL = (('Facebook', 'http://www.facebook.com/decayfilm'),
           ('Twitter', 'http://www.twitter.com/decayfilm'),)
 
+# Custom filters
+from datetime import date
+
+RUN_DATE = date.today()
+def in_future(articles):
+    """Filter to return list of only future articles"""
+    now = RUN_DATE.isoformat()
+    return [article for article in articles if article.date.isoformat() > now]
+
+def in_past(articles):
+    """Filter to return list of only past articles"""
+    now = RUN_DATE.isoformat()
+    return [article for article in articles if article.date.isoformat() <= now]
+
+JINJA_FILTERS = {'in_future': in_future, 'in_past': in_past}
